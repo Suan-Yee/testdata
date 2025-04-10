c899b4a5bb2f3e9bc76e6d6fa9573045f18d810d
Suan-Yee <mgsuansyeey@gmail.com>
2025-04-10 16:54:38 +0630

    'envInfo'

---
+++ envinfo.js
@@ -0,0 +1,106 @@
+const envinfo = {
+    "P0": {
+        "投資家": null,
+        "オペレータ": null,
+        "管理者": [
+            "P0/admin/WEB3AdminLogin_19.jsp",
+            "P0/admin/WEB3AdminLogin_15.jsp",
+            "P0/admin/WEB3AdminLogin_53.jsp"
+        ],
+        "対面": "P0/pl/WEB3OperatorLogin.jsp",
+        "対面ネット": null,
+        "対面オペレータ": null,
+        "リッチ投資家": null,
+        "リッチオペレータ": null
+    },
+    "0D": {
+        "投資家": "0D/pc/WEB3AccountLogin.jsp",
+        "オペレータ": "0D/pc/WEB3OperatorLogin.jsp",
+        "管理者": [
+            "0D/admin/WEB3AdminLogin.jsp"
+        ],
+        "対面": "0D/pl/WEB3OperatorLogin.jsp",
+        "対面ネット": "0D/ppc/WEB3AccountLogin.jsp",
+        "対面オペレータ": "0D/ppc/WEB3OperatorLogin.jsp",
+        "リッチ投資家": null,
+        "リッチオペレータ": null
+    },
+    "0H": {
+        "投資家": null,
+        "オペレータ": null,
+        "管理者": [
+            "0H/admin/WEB3AdminLogin.jsp"
+        ],
+        "対面": null,
+        "対面ネット": null,
+        "対面オペレータ": null,
+        "リッチ投資家": "rc/test_client/RcLogin.jsp",
+        "リッチオペレータ": "rc/test_client/cc_login.jsp"
+    },
+    "08": {
+        "投資家": null,
+        "オペレータ": "08/pc/WEB3OperatorLogin.jsp",
+        "管理者": [
+            "08/admin/WEB3AdminLogin.jsp"
+        ],
+        "対面": null,
+        "対面ネット": null,
+        "対面オペレータ": null,
+        "リッチ投資家": null,
+        "リッチオペレータ": null
+    },
+    "52": {
+        "投資家": "52/pc/WEB3AccountLogin.jsp",
+        "オペレータ": "52/pc/WEB3OperatorLogin.jsp",
+        "管理者": [
+            "52/admin/WEB3AdminLogin.jsp"
+        ],
+        "対面": null,
+        "対面ネット": null,
+        "対面オペレータ": null,
+        "リッチ投資家": null,
+        "リッチオペレータ": null
+    },
+    "44": {
+        "投資家": "44/pc/WEB3AccountLogin.jsp",
+        "オペレータ": "44/pc/WEB3OperatorLogin.jsp",
+        "管理者": [
+            "44/admin/WEB3AdminLogin.jsp"
+        ],
+        "対面": null,
+        "対面ネット": null,
+        "対面オペレータ": null,
+        "リッチ投資家": "rc/test_client/RcLogin.jsp",
+        "リッチオペレータ": "rc/test_client/cc_login.jsp"
+    },
+    "0X": {
+        "投資家": "0X/pc/WEB3AccountLogin.jsp",
+        "オペレータ": "0X/pc/WEB3OperatorLogin.jsp",
+        "管理者": [
+            "0X/admin/WEB3AdminLogin.jsp"
+        ],
+        "対面": null,
+        "対面ネット": null,
+        "対面オペレータ": null,
+        "リッチ投資家": null,
+        "リッチオペレータ": null
+    },
+    "0C": {
+        "投資家": null,
+        "オペレータ": null,
+        "管理者": [
+            "0C/admin/WEB3AdminLogin.jsp"
+        ],
+        "対面": null,
+        "対面ネット": null,
+        "対面オペレータ": null,
+        "リッチ投資家": "rc/test_client/RcLogin.jsp",
+        "リッチオペレータ": "rc/test_client/cc_login.jsp"
+    },
+    "0F": {
+        "投資家": "0F/pc/WEB3AccountLogin.jsp",
+        "オペレータ": "0F/pc/WEB3OperatorLogin.jsp",
+        "管理者": [
+            "0F/admin/WEB3AdminLogin.jsp"
+        ],
+        "対面":
